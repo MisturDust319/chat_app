e262177c5a6d7311c0bb0659115a6a7a75b1caa8
stans <stans@amaterasu.dev>
2017-11-29 22:26:58 -0500

    Starting to incorporate Mongoose

--- server.js
+++ server.js
@@ -36,15 +36,48 @@ MongoClient.connect(mongoURL, (err, database) => {
   })
 });
 
+//helper functions
+//creates a random number from 0-999999999
+//	to be used as a userID
+//	returns the ID generated
+function generateUserID(group_id) {
+	//actually makes the id
+	function getID() {
+		return Math.random() * 999999999;
+	}
+
+	var id = getID();
+
+	//generate a list of current userIDs
+	user_ids = db.groups.find({ _id : group_id }).users.toArray();
+	//keep making a new id until you know it's unique
+	while(id in user_ids ) {
+		id = getID();
+	}
+
+	//insert the new user id to the db
+	//and give them a message array
+	db.groups.update(
+	{
+		"_id" : group_id
+	},
+	{
+		$set : { id : [] }
+	}
+	);
+
+	return id;
+}
+
 //routes
 
 //this method creates a chat group
 app.get('/createGroup', (req, res) => {
 	db.groups.insert({
-		"messages" : {},
+			users : {},
+		});
 	}, (err, new_doc) => {
 		req.session.group_id = new_doc._id;
 		//this callback sets the new group ID in the requester's
 		//	session for future reference
-	});
-})
+});
